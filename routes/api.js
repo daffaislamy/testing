faadb3652e154fa1d1387379b52ef53e41aa95b7
daffaislamy <dfabot07@gmail.com>
2021-07-06 15:00:27 +0700

    first commit

---
+++ routes/api.js
@@ -0,0 +1,178 @@
+__path = process.cwd()
+
+var express = require('express');
+var db = require(__path + '/database/db');
+try {
+var zahirr = db.get("zahirr");
+} catch (e) {
+	console.log('')
+}
+
+var creatorList = ['@daffaislamy_17'];
+var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
+
+
+var ytdl = require('ytdl-core');
+var ytpl = require('ytpl');
+var secure = require('ssl-express-www');
+var cors = require('cors');
+var scrapeYt = require("scrape-yt");
+var fetch = require('node-fetch');
+var cheerio = require('cheerio');
+var request = require('request');
+var TikTokScraper = require('tiktok-scraper');
+var router  = express.Router();
+
+var { color, bgcolor } = require(__path + '/lib/color.js');
+var { fetchJson } = require(__path + '/lib/fetcher.js')
+var options = require(__path + '/lib/options.js');
+var {
+	Vokal,
+	Base,
+	Searchnabi,
+    Gempa
+} = require('./../lib');
+var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
+
+loghandler = {
+    notparam: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter apikey',
+        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6283898698875 , key nya gratis kok gan, jadi santuyy ajaa'
+    },
+    noturl: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter url'
+    },
+    notkata: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter kata'
+    },
+    nottext: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text'
+    },
+    nottext2: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text2'
+    },
+    notnabi: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter nabi'
+    },
+    nottext3: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text3'
+    },
+    nottheme: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter theme'
+    },
+    notusername: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter username'
+    },
+    notvalue: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter value'
+    },
+    notheme: {
+    	status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'theme tidak tersedia silahkan masukkan texmaker/list atau baca documentasi'
+     },
+    invalidlink: {
+        status: false,
+        creator: `${creator}`,
+        message: 'error, mungkin link anda tidak valid.'
+    },
+    invalidkata: {
+        status: false,
+        creator: `${creator}`,
+        message: 'error, mungkin kata tidak ada dalam api.'
+    },
+    error: {
+        status: false,
+        creator: `${creator}`,
+        message: 'mungkin sedang dilakukan perbaikan'
+    }
+}
+
+var len = 15
+        var arr = '123456789abcdefghijklmnopqrstuvwxyz'
+        var random = '';
+
+        for (var i = len; i > 0; i--) {
+            random += arr[Math.floor(Math.random() * arr.length)];
+        }
+
+        var lenn = 5
+        var randomlagi = '';
+
+        for (var i = lenn; i > 0; i--) {
+            randomlagi += arr[Math.floor(Math.random() * arr.length)];
+        }
+
+        var randomTextNumber = random+randomlagi+'---------ZahirGanteng'+'ZHIRRR--GANS';        
+
+})
+/*
+      SOSMED-API
+*/
+router.get('/ig/stalk', async (req, res, next) => {
+        var username = req.query.username            
+	
+    if (!username) return res.json(loghandler.notusername)
+
+       fetch(encodeURI(`https://python-api-zhirrr.herokuapp.com/api/stalk?username=${username}`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data;
+             res.json({
+             	author: 'zeeone',
+                 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+})
+router.get('/youtube/video', async (req, res, next) => {
+        var url = req.query.url            
+	
+    if (!url) return res.json(loghandler.noturl)
+
+       fetch(encodeURI(`https://python-api-zhirrr.herokuapp.com/api/ytv?url=${url}`))
+        .then(response => response.json())
+        .then(data => {
+        var result = data;
+             res.json({
+             	author: 'daffaislamy_17',
+                 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+})
